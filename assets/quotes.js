1cfa65fb8847e3a23cef4b796ec07bf3ad5b0336
burhan1122 <64837183+burhan1122@users.noreply.github.com>
2020-05-10 16:13:53 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "burhan", quotes: "hey kamu, kapan nih ngabuburit bareng:)" },
-    { author: "burhan", quotes: "hey kamu, kapan nih ngabuburit bareng :)" },
-    { author: "burhan", quotes: "hey kamu, kapan nih ngabuburit bareng :)" },
-    { author: "burhan", quotes: "hey kamu, kapan nih ngabuburit bareng :)" },
-    { author: "burhan", quotes: "hey kamu, kapan nih ngabuburit bareng:)" },
-    { author: "burhan", quotes: "hey kamu, kapan nih ngabuburit bareng:)" },
-    { author: "burhan", quotes: "hey kamu, kapan nih ngabuburit bareng  :)" },
+    { author: "Ahmad Burhanuddin", quotes: "jangan berubah ya aku udah sayang kamu" },
+    { author: "Ahmad Burhanuddin", quotes: "jaga kesehatan terus kamu ❤" },
+    { author: "Ahmad Burhanuddin", quotes: "Thanks , For Loving Me❤" },
+    { author: "Ahmad Burhanuddin", quotes: "kapan ketemu lagi udah kangen berat nih" },
+    { author: "Ahmad Burhanuddin", quotes: "kapan aku bisa main ke rumah kamu ?" },
+    { author: "Ahmad Burhanuddin", quotes: "apakah aku bisa menjadi imam kamu ?" },
+    { author: "Ahmad Burhanuddin", quotes: "ILY , so much dear ❤" },
 ];
